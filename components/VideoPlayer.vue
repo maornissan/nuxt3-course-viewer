c6826f4a38f108bbc615b01671273262b56dee5c
maornissan <mr.nisan@gmail.com>
2023-02-04 08:44:06 +0200

    initial

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <iframe
+    width="560"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    height="315"
+    title="Video Player"
+    frameborder="0"
+    allow="autoplay"
+    allowfullscreen
+  ></iframe>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+<style scoped></style>
---
+++ pages/course.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+  >
+    <div class="prose mb-12">
+      <h1>
+        <span class="font-medium">
+          Course:
+          <span class="font-bold">Mastering Nuxt 3</span>
+        </span>
+      </h1>
+    </div>
+
+    <div class="flex flex-row justify-center flex-grow">
+      <div
+        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+      >
+        <h3>Chapters</h3>
+        <!-- All the lessons for the course listed here -->
+        <div
+          class="flex flex-col mb-4 space-y-1"
+          v-for="chapter in chapters"
+          :key="chapter.slug"
+        >
+          <h4>{{ chapter.title }}</h4>
+          <NuxtLink
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            :to="`${lesson.path}`"
+            :class="{ 'text-blue-500': lesson.path === $route.fullPath }"
+          >
+            Lesson {{ index + 1 }}: {{ lesson.title }}</NuxtLink
+          >
+        </div>
+      </div>
+
+      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const { chapters } = useCourse();
+</script>
